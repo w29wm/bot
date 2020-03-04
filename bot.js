45c05f275f135affe2560bc9d85e30bc725b817d
w29wm <61799452+w29wm@users.noreply.github.com>
2020-03-04 20:20:02 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,100 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+token = Njg0ODE1OTUwODk3MzQ4NzA2.Xl_nYw.BTMExrZvofDk-h02Mt4YgNereEU
+
+
+
+
+owner = 684815950897348706
+
+
+prefix = "Y"
+
+
+
+game = "DEFAULT"
+
+
+
+status = ONLINE
+
+
+songinstatus=false
+
+
+altprefix = "NONE"
+
+
+success = "🎶"
+warning = "💡"
+error = "🚫"
+loading = "⌚"
+searching = "🔎"
+
+
+
+help = help
+
+
+
+npimages = false
+
+
+
+stayinchannel = true
+
+
+
+maxtime = 0
+
+
+
+playlistsfolder = "Playlists"
+
+
+
+
+updatealerts=true
+
+
+
+lyrics.default = "A-Z Lyrics"
+
+///الاختصارات
+
+aliases {
+  // General commands
+  settings = [ status ]
+
+  // Music commands
+  lyrics = [ l ]
+  nowplaying = [ np, current ]
+  play = [ p ]
+  playlists = [ pls ]
+  queue = [ list ]
+  remove = [ delete ]
+  scsearch = []
+  search = [ ytsearch ]
+  shuffle = []
+  skip = [ voteskip ]
+
+  // Admin commands
+  prefix = [ setprefix ]
+  setdj = []
+  settc = []
+  setvc = []
+
+  // DJ Commands
+  forceremove = [ forcedelete, modremove, moddelete ]
+  forceskip = [ modskip ]
+  movetrack = [ move ]
+  pause = []
+  playnext = []
+  repeat = []
+  skipto = [ jumpto ]
+  stop = []
+  volume = [ vol ]
+}
+
+
+eval=false
